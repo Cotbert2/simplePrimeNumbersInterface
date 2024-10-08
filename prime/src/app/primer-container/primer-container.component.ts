5c67f282c2360af75460277902b2990b6ee335a9
Cotbert <mateojgdev@gmail.com>
2024-10-08 16:30:10 -0500

    primeNumberSimpleApp V1.0.0

--- prime/src/app/primer-container/primer-container.component.ts
+++ prime/src/app/primer-container/primer-container.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, numberAttribute, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-primer-container',
@@ -7,6 +7,47 @@ import { Component } from '@angular/core';
   templateUrl: './primer-container.component.html',
   styleUrl: './primer-container.component.css'
 })
-export class PrimerContainerComponent {
+export class PrimerContainerComponent implements OnInit{
+
+
+  currentNumber : number = 2;
+
+  ngOnInit(): void {
+    console.log(this.isPrime(6))
+
+  }
+
+  getNextNumber(primeNumber : number){
+    let currentNumberToCheck : number = ++primeNumber;
+
+    while(!this.isPrime(currentNumberToCheck))  currentNumberToCheck++;
+    return currentNumberToCheck;
+  }
+
+  getPreviousNumber(primeNumber : number){
+    if (primeNumber === 2) return primeNumber;
+    let currentNumberToCheck : number = --primeNumber;
+
+    while(!this.isPrime(currentNumberToCheck))  currentNumberToCheck--;
+    return currentNumberToCheck;
+  }
+
+  isPrime (num : number) : boolean {
+    for (let i = num - 1; i > 1; i --)
+      if(num % i == 0) return false
+    return true;
+  }
+
+  forwardNumber(event : MouseEvent) : void {
+    event.preventDefault(); //prevent browser menu
+    console.log('right click');
+    this.currentNumber = this.getNextNumber(this.currentNumber);
+  }
+
+  backwardNumber() : void {
+    console.log('left click');
+    this.currentNumber = this.getPreviousNumber(this.currentNumber);
+
+  }
 
 }
